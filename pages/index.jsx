c596243cb2924c6b57b1d96f055b2bf0ffa4eb57
Vladimir <turkonjavlada@gmail.com>
2021-04-05 16:54:38 +0200

    Update home page to show if the user is signed up or not

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import { buildClient } from '../api/build-client';
 
 const Home = ({ currentUser }) => {
-  console.log('CURRENT USER: ', currentUser);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>
+        {currentUser ? `Hi ${currentUser.email}!` : `You're not signed in`}
+      </h1>
     </div>
   );
 };
