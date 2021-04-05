cac7806c8fbaf8feef373b7c0811e9fb14d3b3b4
Vladimir <turkonjavlada@gmail.com>
2021-04-05 18:07:55 +0200

    Add a header component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import App from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import { buildClient } from '../api/build-client';
+import { Header } from '../components/header';
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </>
   );
